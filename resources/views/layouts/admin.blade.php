75cd7aa8ba9026e17078bc1c53879daff7e5928a
teknisi <teknisi_ti@skmbali.sch.id>
2020-03-07 11:20:58 +0700

    edit meneh

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -162,7 +162,7 @@
     <a href="index3.html" class="brand-link">
       <img src="{{asset('adminjoss')}}/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+      <span class="brand-text font-weight-light">Joss Mart</span>
     </a>
 
     <!-- Sidebar -->
